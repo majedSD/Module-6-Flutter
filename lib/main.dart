9cac82ca436df4f67a2043f6f7137b24f2f65ad0
Ekushey Computer <majedulislam117@gmail.com>
2023-09-13 12:58:01 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,156 @@
+/**
+ *
+ *
+ *
+ *    KOMAIYA KOTHA KOW MIA MOSTANI KORO NI AMAR LOGE MOSTANI KORIA O NAH AMARA CINO NI
+ *    EKABARE DUKAI DIMU TUMARA JELHAZOTO MOJA BUJILAIBAI KHAHINI AMAR LOGE KORIO NAH
+ *
+ *
+ *
+ *
+ */
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+///My first programme is running now and how work my flutter project and what kind of benifit here
+void main(){
+  runApp( const MyApp());
 }
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeActivity(),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomeActivity extends StatelessWidget{
+  const HomeActivity({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Home page',style: TextStyle(fontSize: 40),),
+        backgroundColor: Colors.black,
+        centerTitle: true,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              child: Text('Hello world'),
+              alignment: Alignment.center,
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: Colors.purple,
+                border: Border.all(color: Colors.black87,width: 2),
+                //borderRadius: BorderRadius.circular(20)
+                borderRadius: BorderRadius.only(topRight: Radius.circular(20),bottomLeft: Radius.circular(20)),
+                //shape: BoxShape.rectangle,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: (){print('Sign into email button');},
+              onLongPress: (){print('Sign out on email button');},
+              child: Text('SEND EMAIL'),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.pink,
+                  textStyle: TextStyle(fontSize: 30,fontWeight: FontWeight.bold ),
+                  shape:RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  )
+              ),
+            ),
+            ///This programme is about text button and how work this text button
+            SizedBox(height: 10,),
+            TextButton(
+              onPressed: (){print('Resend your email');},
+              onLongPress: (){print('Not Resend your email');},
+              child: Text('RESEND EMAIL'),
+              style: TextButton.styleFrom(
+                  foregroundColor: Colors.deepOrange,
+                  textStyle: TextStyle(fontSize: 24,fontWeight: FontWeight.bold)
+              ),
+            ),
+            IconButton(onPressed:(){}, icon: Icon(Icons.add)),
+            SizedBox(height: 10,),
+            OutlinedButton(onPressed: (){}, child: Text('OUTLINED BUTTON')),
+            SizedBox(height: 10,),
+            SizedBox(
+              width: 300,
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+                decoration: InputDecoration(
+                    fillColor: Colors.black,
+                    filled: true,
+                    hintText: 'Enter your email',
+                    hintStyle: TextStyle(
+                      color: Colors.white60,
+                    ),
+                    suffixIcon: Icon(Icons.email),
+                    suffixIconColor: Colors.white60,
+                    label: Text('Enter your email'),
+                    labelStyle: TextStyle(color: Colors.white60),
+                    border:OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green),
+                    ),
+                    disabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.yellow),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.purple),
+                    )
+                ),
+              ),
+            ),
+            SizedBox(height: 10,),
+            SizedBox(///Same padding dara wrap kora jai and sizedbox er moto same kaj kore
+              ///padding used korle left right teka width reduce kora jai
+              ///Textfield ka padding dara wrap korea
+              width: 300,
+              child: TextField(
+                obscureText: true,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+                decoration: InputDecoration(
+                    fillColor: Colors.black,
+                    filled: true,
+                    hintText: 'Password',
+                    hintStyle: TextStyle(
+                      color: Colors.white60,
+                    ),
+                    suffixIcon: Icon(Icons.password),
+                    suffixIconColor: Colors.white60,
+                    label: Text('Password'),
+                    labelStyle: TextStyle(color: Colors.white60),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green),
+                    ),
+                    disabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.yellow),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.purple),
+                    )
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
